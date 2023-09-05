7aba1a624a08fbc658a31b8995bdd76ecafa1422
Osama Alaa <127636327+osamaalaa197@users.noreply.github.com>
2023-09-05 11:06:41 +0300

    Delete ReactNativeFrontEnd directory

--- ReactNativeFrontEnd/Screens/Cart.jsx
+++
@@ -1,9 +0,0 @@
-// import React from "react";
-// import { View,Text } from "react-native";
-// import { SafeAreaView } from "react-native-safe-area-context";
-
-// export default function Cart() {
-//     <SafeAreaView>
-//     <Text>Cart</Text>
-//   </SafeAreaView>
-// }
